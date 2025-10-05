e4a82b11a6e0ec9d9d52dcb4410d7789fc4c3ef5
Charalampos Polychronakis <polychronakis.h@gmail.com>
2025-10-05 17:55:03 +0200

    [Sprint 1][Task 5]: Add library crate and integration tests structure

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,34 @@
+use std::error::Error;
+
+use axum::serve::Serve;
+use axum::Router;
+use tower_http::services::ServeDir;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    // Address is pub so tests know it
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = Router::new()
+        .nest_service("/", ServeDir::new("assets"));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        let app = Application {
+            server: server,
+            address: address
+        };
+
+        Ok(app)
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
